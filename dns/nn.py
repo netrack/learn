3f0f6f631c6dbf65b0ea329d6b2e21604e313784
Yasha Bubnov <ybubnov@gojuno.com>
2018-03-30 16:09:30 +0300

    Add precision and recall metrics.

--- dns/nn.py
+++ dns/nn.py
@@ -3,6 +3,9 @@ import csv
 import contextlib
 import random
 import numpy
+import keras
+
+from keras import backend as K
 
 
 def first(tuples):
@@ -11,17 +14,80 @@ def first(tuples):
 
 
 def last(tuples):
-    """Retur a numpy array with the last elements of each tuple."""
+    """Return a numpy array with the last elements of each tuple."""
     return numpy.array([t[1:] for t in tuples])
 
 
-def train_and_estimate(dim, num_classes, units, total, batch_size, filenames):
-    import keras
+class Recall(keras.layers.Layer):
 
+    def __init__(self, name="recall", **kwargs):
+        super().__init__(name=name, **kwargs)
+        self.true_positives = K.variable(0, dtype="int32")
+        self.false_negatives = K.variable(0, dtype="int32")
+
+    def reset_states(self):
+        K.set_value(self.true_positives, 0)
+        K.set_value(self.false_negatives, 0)
+
+    def __call__(self, y_true, y_pred):
+        y_true = K.cast(y_true, "int32")
+        y_pred = K.cast(K.round(y_pred), "int32")
+        neg_y_pred = 1 - y_pred
+
+        true_positives = K.sum(y_true * y_pred)
+        false_negatives = K.sum(y_true * neg_y_pred)
+
+        current_true_positives = self.true_positives * 1
+        current_false_negatives = self.false_negatives * 1
+
+        self.add_update(K.update_add(self.true_positives, true_positives), inputs=[y_true, y_pred])
+        self.add_update(K.update_add(self.false_negatives, false_negatives), inputs=[y_true, y_pred])
+
+        tp = current_true_positives + true_positives
+        fn = current_false_negatives + false_negatives
+
+        return tp / (tp + fn)
+
+
+class Precision(keras.layers.Layer):
+
+    def __init__(self, name="precision", **kwargs):
+        super().__init__(name=name, **kwargs)
+        self.true_positives = K.variable(0, dtype="int32")
+        self.false_positives = K.variable(0, dtype="int32")
+
+    def reset_states(self):
+        K.set_value(self.true_positives, 0)
+        K.set_value(self.false_positives, 0)
+
+    def __call__(self, y_true, y_pred):
+        y_true = K.cast(y_true, "int32")
+        y_pred = K.cast(K.round(y_pred), "int32")
+        neg_y_true = 1 - y_true
+
+        true_positives = K.sum(y_true * y_pred)
+        false_positives = K.sum(neg_y_true * y_pred)
+
+        current_true_positives = self.true_positives * 1
+        current_false_positives = self.false_positives * 1
+
+        self.add_update(K.update_add(self.true_positives, true_positives), inputs=[y_true, y_pred])
+        self.add_update(K.update_add(self.false_positives, false_positives), inputs=[y_true, y_pred])
+
+        tp = current_true_positives + true_positives
+        fp = current_false_positives + false_positives
+
+        return tp / (tp + fp)
+
+
+def train_and_estimate(dim, num_classes, units, total, batch_size, filenames):
     layer1 = int(units[0])
     layer2 = int(units[2])
     layer3 = int(units[3])
     layer4 = int(units[4])
+
+    recall = Recall()
+    precision = Precision()
 
     model = keras.models.Sequential()
     model.add(keras.layers.Dense(layer1, activation="relu", input_dim=dim))
@@ -34,7 +100,7 @@ def train_and_estimate(dim, num_classes, units, total, batch_size, filenames):
 
     model.compile(loss="categorical_crossentropy",
                   optimizer="nadam",
-                  metrics=["accuracy"])
+                  metrics=["accuracy", recall, precision])
 
     with contextlib.ExitStack() as stack:
         new = lambda name: stack.enter_context(open(name))
@@ -71,9 +137,8 @@ def train_and_estimate(dim, num_classes, units, total, batch_size, filenames):
         y_batch = keras.utils.to_categorical(first(batch), num_classes)
 
         loss_and_metrics = model.evaluate(x_batch, y_batch)
-        accuracy = loss_and_metrics[1]
 
-    return accuracy
+    return loss_and_metrics[1:]
 
 
 def main():
