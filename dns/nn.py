9c5721afdd12565e30116a187ec459600133eb0a
Yasha Bubnov <ybubnov@gojuno.com>
2018-08-01 10:05:36 +0300

    Switch to "keras_metrics" package.

--- dns/nn.py
+++ dns/nn.py
@@ -5,6 +5,7 @@ import random
 import numpy
 import os.path
 import keras
+import keras_metrics
 import scipy
 
 import matplotlib.pyplot as plt
@@ -29,83 +30,20 @@ def last(tuples):
     return numpy.array([t[1:] for t in tuples])
 
 
-class Recall(keras.layers.Layer):
-
-    def __init__(self, name="recall", **kwargs):
-        super().__init__(name=name, **kwargs)
-        self.true_positives = K.variable(0, dtype="int32")
-        self.false_negatives = K.variable(0, dtype="int32")
-
-    def reset_states(self):
-        K.set_value(self.true_positives, 0)
-        K.set_value(self.false_negatives, 0)
-
-    def __call__(self, y_true, y_pred):
-        y_true = K.cast(y_true, "int32")
-        y_pred = K.cast(K.round(y_pred), "int32")
-        neg_y_pred = 1 - y_pred
-
-        true_positives = K.sum(y_true * y_pred)
-        false_negatives = K.sum(y_true * neg_y_pred)
-
-        current_true_positives = self.true_positives * 1
-        current_false_negatives = self.false_negatives * 1
-
-        self.add_update(K.update_add(self.true_positives, true_positives), inputs=[y_true, y_pred])
-        self.add_update(K.update_add(self.false_negatives, false_negatives), inputs=[y_true, y_pred])
-
-        tp = current_true_positives + true_positives
-        fn = current_false_negatives + false_negatives
-
-        return tp / (tp + fn)
-
-
-class Precision(keras.layers.Layer):
-
-    def __init__(self, name="precision", **kwargs):
-        super().__init__(name=name, **kwargs)
-        self.true_positives = K.variable(0, dtype="int32")
-        self.false_positives = K.variable(0, dtype="int32")
-
-    def reset_states(self):
-        K.set_value(self.true_positives, 0)
-        K.set_value(self.false_positives, 0)
-
-    def __call__(self, y_true, y_pred):
-        y_true = K.cast(y_true, "int32")
-        y_pred = K.cast(K.round(y_pred), "int32")
-        neg_y_true = 1 - y_true
-
-        true_positives = K.sum(y_true * y_pred)
-        false_positives = K.sum(neg_y_true * y_pred)
-
-        current_true_positives = self.true_positives * 1
-        current_false_positives = self.false_positives * 1
-
-        self.add_update(K.update_add(self.true_positives, true_positives), inputs=[y_true, y_pred])
-        self.add_update(K.update_add(self.false_positives, false_positives), inputs=[y_true, y_pred])
-
-        tp = current_true_positives + true_positives
-        fp = current_false_positives + false_positives
-
-        return tp / (tp + fp)
-
-
 def train_and_estimate(dim, num_classes, units, total, batch_size, filenames):
     layer1 = int(units[0])
     layer2 = int(units[2])
     layer3 = int(units[3])
     layer4 = int(units[4])
 
-    recall = Recall()
-    precision = Precision()
+    recall = keras_metrics.recall()
+    precision = keras_metrics.precision()
 
     model = keras.models.Sequential()
     model.add(keras.layers.Dense(layer1, activation="relu", input_dim=dim))
     model.add(keras.layers.Dropout(units[1]))
     model.add(keras.layers.Dense(layer2, activation="relu"))
     model.add(keras.layers.Dense(layer3, activation="tanh"))
-    # model.add(keras.layers.Dense(layer4, activation="relu"))
     model.add(keras.layers.Dropout(units[5]))
     model.add(keras.layers.Dense(num_classes, activation="softmax"))
 
@@ -137,7 +75,9 @@ def train_and_estimate(dim, num_classes, units, total, batch_size, filenames):
             x_batch = last(batch)
             y_batch = keras.utils.to_categorical(first(batch), num_classes)
 
-            model.train_on_batch(x_batch, y_batch)
+            # model.train_on_batch(x_batch, y_batch)
+            history = model.fit(x_batch, y_batch, verbose=1)
+            print(history)
 
         # Evaluate model accuracy.
         batch = []
@@ -151,7 +91,7 @@ def train_and_estimate(dim, num_classes, units, total, batch_size, filenames):
         y_true = first(batch)
 
         y_batch = keras.utils.to_categorical(y_true, num_classes)
-        loss_and_metrics = model.evaluate(x_batch, y_batch)
+        loss_and_metrics = model.evaluate(x_batch, y_batch, verbose=2)
 
         y_score = numpy.array(model.predict_proba(x_batch))
         y_batch = numpy.array(y_batch)
@@ -245,7 +185,7 @@ def main():
         f.write("recall = {0}\n".format(metrics[1]))
         f.write("precision = {0}\n".format(metrics[2]))
 
-    plot(x_true, y_true, y_score, labels, num_classes)
+    ## plot(x_true, y_true, y_score, labels, num_classes)
 
 
 if __name__ == "__main__":
